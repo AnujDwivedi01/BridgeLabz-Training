dde75105d66e8b036003f15689960a8abe443da8
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2025-12-24 01:04:35 +0530

    [Anuj] : Adding Prime Number Checker program

---
+++ gcr-codebase/additional_program/PrimeNumberChecker.java
@@ -0,0 +1,40 @@
+// java program to check a prime number
+
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+
+    // method to check whether a number is prime
+    public static boolean isPrime(int number) {
+
+        // numbers less than or equal to 1 are not prime
+        if (number <= 1) {
+            return false;
+        }
+
+        // Check divisibility from 2 to sqrt(number)
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false; // Not prime
+            }
+        }
+
+        return true; // Prime number
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+		
+        // calling method and printing result
+        System.out.print("Enter a number to check if it is prime: ");
+        int number = input.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a Prime number.");
+        } else {
+            System.out.println(number + " is NOT a Prime number.");
+        }
+
+        input.close();
+    }
+}
