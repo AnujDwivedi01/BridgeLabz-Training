209d4b3c99cf2748c3697033782fbe71677a696f
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2025-12-24 01:02:43 +0530

    [Anuj] : Adding Fibonacci Sequence program

---
+++ gcr-codebase/additional_program/FibonacciSequence.java
@@ -0,0 +1,41 @@
+// java program to generate fibonacci series
+
+import java.util.Scanner;
+
+public class FibonacciSequence {
+
+    // method to generate and print Fibonacci sequence
+    public static void generateFibonacci(int terms) {
+
+        int first = 0;
+        int second = 1;
+
+        System.out.println("Fibonacci Sequence:");
+
+        for (int i = 1; i <= terms; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int terms = input.nextInt();
+		
+        // calling method and printing result
+        if (terms <= 0) {
+            System.out.println("Please enter a positive number.");
+        } else {
+            generateFibonacci(terms);
+        }
+
+        input.close();
+    }
+}
