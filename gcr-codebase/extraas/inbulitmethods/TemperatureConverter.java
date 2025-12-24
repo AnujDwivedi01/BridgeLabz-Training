1757d54f200050fbae8f0ddb21974550f5a36a89
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2025-12-24 19:08:24 +0530

    [Anuj] : add temperature converter program

--- gcr-codebase/extraas/inbulitmethods/TemperatureConverter.java
+++ gcr-codebase/extraas/inbulitmethods/TemperatureConverter.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class TemperatureConverter {
 
-    // method to convert Fahrenheit to Celsius
+    // method to convert Fahrenheit to Celsius temperature
     public static double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
