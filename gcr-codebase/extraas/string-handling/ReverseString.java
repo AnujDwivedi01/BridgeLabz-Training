788e96941fbb150d9ff3b917c315a6a0521bcf15
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2025-12-24 16:24:23 +0530

    [Anuj] : added program of string handling

---
+++ gcr-codebase/extraas/string-handling/ReverseString.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+    // Method to reverse a string
+    public static String reverse(String text) {
+        String result = "";
+
+        for (int i = text.length() - 1; i >= 0; i--) {
+            result += text.charAt(i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = input.next();
+
+        System.out.println("Reversed String: " + reverse(text));
+    }
+}
