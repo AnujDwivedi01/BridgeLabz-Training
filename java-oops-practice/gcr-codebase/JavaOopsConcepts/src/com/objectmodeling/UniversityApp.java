80be1c625d74da2fd68c560ce17a91b775ad9ca5
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2025-12-30 22:54:14 +0530

    [Anuj] : added university app file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/UniversityApp.java
@@ -0,0 +1,53 @@
+package com.objectmodeling;
+class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
+
+
+class Student {
+    private String studentName;
+
+    public Student(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void enrollCourse(Course course) {
+        System.out.println(studentName + " enrolled in " + course.getCourseName());
+    }
+}
+
+
+class Professor {
+    private String professorName;
+
+    public Professor(String professorName) {
+        this.professorName = professorName;
+    }
+
+    public void assignProfessor(Course course) {
+        System.out.println(professorName + " is teaching " + course.getCourseName());
+    }
+}
+
+
+public class UniversityApp {
+	public static void main(String[] args) {
+
+        Course course = new Course("Java Programming");
+
+        Student student = new Student("Rahul");
+        Professor professor = new Professor("Dr. Singh");
+
+        student.enrollCourse(course);
+        professor.assignProfessor(course);
+    }
+
+}
