991d664daac93dfb6b8090aa0a1361cf31b38538
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2025-12-31 15:36:40 +0530

    [Anuj] : added employee hierarchy file

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/Managers.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/employee/Managers.java
@@ -1,5 +1,20 @@
 package com.inheritance.assistedproblems.employee;
 
-public class Managers {
+class Managers extends Employees{
+	  public Managers(String name, int id, int salary) {
+		  super(name,id,salary); // parent class constructor
+	  }
+	
+}
 
+class Developer extends Employees{
+	public Developer(String name, int id, int salary) {
+		  super(name,id,salary); // parent class constructor
+	  }
+}
+
+class Intern extends Employees{
+	public Intern(String name, int id, int salary) {
+		  super(name,id,salary); // parent class constructor
+	  }
 }
