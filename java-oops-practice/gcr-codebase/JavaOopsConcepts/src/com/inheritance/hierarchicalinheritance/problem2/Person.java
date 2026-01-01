478a58430b38c5ac31796f49d38efa98ddd6fcb6
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-01 11:19:31 +0530

    [Anuj] : added school app file

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem2/Person.java
@@ -0,0 +1,13 @@
+package com.inheritance.hierarchicalinheritance.problem2;
+
+class Person {
+
+    protected String name;
+    protected int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
