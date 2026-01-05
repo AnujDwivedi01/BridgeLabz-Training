5f42078b51e1be6d19da0bdd7ec1a28866c96a93
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-05 14:58:56 +0530

    [Anuj] : added pair with given sum  program

--- java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmap/PairWithGivenSum.java
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmap/PairWithGivenSum.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class PairWithGivenSum {
 
-    public static void main(String[] args) {
+         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter size of array: ");
