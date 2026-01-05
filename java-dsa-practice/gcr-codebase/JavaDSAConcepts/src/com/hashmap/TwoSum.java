4f881bf119601010f9030f09aec444b3bf5a58ce
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-05 14:57:17 +0530

    [Anuj] : added two sum program

--- java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmap/TwoSum.java
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/hashmap/TwoSum.java
@@ -7,7 +7,7 @@ public class TwoSum {
     public static int[] twoSum(int[] arr, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             int needed = target - arr[i];
 
             if (map.containsKey(needed)) {
