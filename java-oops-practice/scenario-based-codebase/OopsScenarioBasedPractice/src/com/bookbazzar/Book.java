400e51dc6a30549b4b06fb7a9d5d213d48329274
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-07 15:03:49 +0530

    [Anuj] : added book bazzar app file

--- java-oops-practice/scenario-based-codebase/OopsScenarioBasedPractice/src/com/bookbazzar/Book.java
+++ java-oops-practice/scenario-based-codebase/OopsScenarioBasedPractice/src/com/bookbazzar/Book.java
@@ -1,39 +1,39 @@
 package com.bookbazzar;
+
 // Base Book class
 abstract class Book implements IDiscountable {
 
-    protected String title;
-    protected String author;
-    protected double price;
+	protected String title;
+	protected String author;
+	protected double price;
 
-    // Stock should not be modified directly
-    private int stock;
+	// Stock should not be modified directly
+	private int stock;
 
-    // Constructor without offer
-    public Book(String title, String author, double price, int stock) {
-        this.title = title;
-        this.author = author;
-        this.price = price;
-        this.stock = stock;
-    }
+	// Constructor without offer
+	public Book(String title, String author, double price, int stock) {
+		this.title = title;
+		this.author = author;
+		this.price = price;
+		this.stock = stock;
+	}
 
-    // Encapsulated stock access
-    public int getStock() {
-        return stock;
-    }
+	// Encapsulated stock access
+	public int getStock() {
+		return stock;
+	}
 
-    // Controlled stock update
-    public void reduceStock(int quantity) {
-        if (quantity <= stock) {
-            stock -= quantity;
-        } else {
-            throw new RuntimeException("Insufficient stock for " + title);
-        }
-    }
+	// Controlled stock update
+	public void reduceStock(int quantity) {
+		if (quantity <= stock) {
+			stock -= quantity;
+		} else {
+			throw new RuntimeException("Insufficient stock for " + title);
+		}
+	}
 
-    // Common book details
-    public void displayBook() {
-        System.out.println(title + " by " + author + " | Price: ₹" + price +
-                           " | Stock: " + stock);
-    }
+	// Common book details
+	public void displayBook() {
+		System.out.println(title + " by " + author + " | Price: ₹" + price + " | Stock: " + stock);
+	}
 }
