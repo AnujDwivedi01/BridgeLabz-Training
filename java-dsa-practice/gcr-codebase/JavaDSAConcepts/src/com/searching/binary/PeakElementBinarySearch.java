004f8cdcfb09130ca224e3ca553981c34917d60e
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-07 21:33:31 +0530

    [Anuj] : added peek elemnent by binary search rpogram

--- java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/searching/binary/PeakElementBinarySearch.java
+++ java-dsa-practice/gcr-codebase/JavaDSAConcepts/src/com/searching/binary/PeakElementBinarySearch.java
@@ -1,38 +1,39 @@
 package com.searching.binary;
+
 public class PeakElementBinarySearch {
 
-    public static int findPeakElement(int[] arr) {
-        int left = 0;
-        int right = arr.length - 1;
+	public static int findPeakElement(int[] arr) {
+		int left = 0;
+		int right = arr.length - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
 
-            boolean leftSmaller = (mid == 0) || (arr[mid] > arr[mid - 1]);
-            boolean rightSmaller = (mid == arr.length - 1) || (arr[mid] > arr[mid + 1]);
+			boolean leftSmaller = (mid == 0) || (arr[mid] > arr[mid - 1]);
+			boolean rightSmaller = (mid == arr.length - 1) || (arr[mid] > arr[mid + 1]);
 
-            // Check if mid is a peak
-            if (leftSmaller && rightSmaller) {
-                return mid;
-            }
-            // If left neighbor is greater, move left
-            else if (mid > 0 && arr[mid] < arr[mid - 1]) {
-                right = mid - 1;
-            }
-            // Else move right
-            else {
-                left = mid + 1;
-            }
-        }
-        return -1; // should never reach here for valid input
-    }
+			// Check if mid is a peak
+			if (leftSmaller && rightSmaller) {
+				return mid;
+			}
+			// If left neighbor is greater, move left
+			else if (mid > 0 && arr[mid] < arr[mid - 1]) {
+				right = mid - 1;
+			}
+			// Else move right
+			else {
+				left = mid + 1;
+			}
+		}
+		return -1; // should never reach here for valid input
+	}
 
-    public static void main(String[] args) {
-        int[] arr = {1, 3, 20, 4, 1, 0};
+	public static void main(String[] args) {
+		int[] arr = { 1, 3, 20, 4, 1, 0 };
 
-        int peakIndex = findPeakElement(arr);
+		int peakIndex = findPeakElement(arr);
 
-        System.out.println("Peak element index: " + peakIndex);
-        System.out.println("Peak element value: " + arr[peakIndex]);
-    }
+		System.out.println("Peak element index: " + peakIndex);
+		System.out.println("Peak element value: " + arr[peakIndex]);
+	}
 }
