5c683ceef925d7d2dafad4dba684ba373b0b7097
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-10 13:21:55 +0530

    [Anuj] : added library organizer app file

--- java-dsa-practice/scenario-based-codebase/DSAScenarioBasedPractice/src/com/libraryorganizer/BookShelfApp.java
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBasedPractice/src/com/libraryorganizer/BookShelfApp.java
@@ -3,22 +3,22 @@ package com.libraryorganizer;
 public class BookShelfApp {
 	public static void main(String[] args) {
 
-        Library library = new Library();
+		Library library = new Library();
 
-        Book b1 = new Book("The Hobbit", "J.R.R. Tolkien");
-        Book b2 = new Book("Harry Potter", "J.K. Rowling");
-        Book b3 = new Book("Clean Code", "Robert Martin");
+		Book b1 = new Book("The Hobbit", "J.R.R. Tolkien");
+		Book b2 = new Book("Harry Potter", "J.K. Rowling");
+		Book b3 = new Book("Clean Code", "Robert Martin");
 
-        library.addBook("Fantasy", b1);
-        library.addBook("Fantasy", b2);
-        library.addBook("Programming", b3);
+		library.addBook("Fantasy", b1);
+		library.addBook("Fantasy", b2);
+		library.addBook("Programming", b3);
 
-        library.addBook("Fantasy", b1); // duplicate
+		library.addBook("Fantasy", b1); // duplicate
 
-        library.displayCatalog();
+		library.displayCatalog();
 
-        library.borrowBook("Fantasy", b2);
+		library.borrowBook("Fantasy", b2);
 
-        library.displayCatalog();
-    }
+		library.displayCatalog();
+	}
 }
