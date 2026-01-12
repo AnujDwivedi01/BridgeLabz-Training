24c13936f824e0e5f0f5df41e986598256039aa4
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-12 16:29:07 +0530

    [Anuj] : added text editor app program

---
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBasedPractice/src/com/texteditor/Action.java
@@ -0,0 +1,12 @@
+package com.texteditor;
+
+public class Action {
+	String type;
+	String text;
+
+	public Action(String type, String text) {
+		this.type = type;
+		this.text = text;
+	}
+
+}
---
+++ java-dsa-practice/scenario-based-codebase/DSAScenarioBasedPractice/src/com/texteditor/EditorApp.java
@@ -0,0 +1,25 @@
+package com.texteditor;
+
+public class EditorApp {
+
+	public static void main(String[] args) {
+		TextEditor editor = new TextEditor();
+
+		editor.insert("Hello ");
+		editor.insert("World");
+		editor.showContent();
+		
+		
+        editor.delete(5);
+        editor.showContent();
+
+        editor.undo();
+        editor.showContent();
+
+        editor.redo();
+        editor.showContent();
+
+
+	}
+
+}
