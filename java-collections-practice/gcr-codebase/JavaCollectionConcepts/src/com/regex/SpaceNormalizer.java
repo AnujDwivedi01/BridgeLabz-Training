fbd75b9839a488f9e457b6951a787c80af65ae3a
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-23 20:35:09 +0530

    [Anuj] : added space normalizer  file program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/SpaceNormalizer.java
@@ -0,0 +1,13 @@
+package com.regex;
+
+public class SpaceNormalizer {
+
+	public static void main(String[] args) {
+
+		String input = "This   is    an example   with   multiple   spaces.";
+
+		String result = input.replaceAll("\\s+", " ");
+
+		System.out.println(result);
+	}
+}
