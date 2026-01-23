9728329296ebf0fb8f40c2211bec09b1999d88d4
Anuj Dwivedi <anujdwivedi2759@gmail.com>
2026-01-23 20:49:31 +0530

    [Anuj] : added SSN validator file program

---
+++ java-collections-practice/gcr-codebase/JavaCollectionConcepts/src/com/regex/SSNValidator.java
@@ -0,0 +1,26 @@
+package com.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SSNValidator {
+
+	public static void main(String[] args) {
+
+		String text = "My SSN is 123-45-6789.";
+
+		String regex = "\\b\\d{3}-\\d{2}-\\d{4}\\b";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(text);
+
+		if (matcher.find()) {
+			System.out.println("" + matcher.group() + "\" is valid");
+		} else {
+			System.out.println(" Invalid SSN");
+		}
+
+		// Direct validation examples
+		System.out.println("123-45-6789".matches(regex)); // true
+		System.out.println("123456789".matches(regex)); // false
+	}
+}
